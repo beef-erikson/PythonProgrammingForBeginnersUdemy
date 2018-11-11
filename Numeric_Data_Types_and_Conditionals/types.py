8d6a73b8217c58810ffe3e1a05cc55b5f83793b8
Troy Martin <beef.erikson.studios@gmail.com>
2018-11-10 23:40:38 -0500

    added bool
    Signed-off-by: Troy Martin <beef.erikson.studios@gmail.com>

--- Numeric_Data_Types_and_Conditionals/types.py
+++ Numeric_Data_Types_and_Conditionals/types.py
@@ -3,8 +3,10 @@
 
 number = 5
 value = 2.5
+is_triggered = True
 
-print(type(number))     # int
-print(type(value))      # float
-print(type("wheee"))    # str
-print(type(6/2))        # float
+print(type(number))         # int
+print(type(value))          # float
+print(type(is_triggered))   # bool
+print(type("wheee"))        # str
+print(type(6/2))            # float
