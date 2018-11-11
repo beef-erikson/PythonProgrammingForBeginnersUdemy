188f904bb10a07e8547de82903ba0e39d6465edd
Troy Martin <beef.erikson.studios@gmail.com>
2018-11-10 23:11:19 -0500

    function return pointer set to None
    Signed-off-by: Troy Martin <beef.erikson.studios@gmail.com>

--- Numeric_Data_Types_and_Conditionals/simple_interest.py
+++ Numeric_Data_Types_and_Conditionals/simple_interest.py
@@ -2,7 +2,7 @@
 """
 
 
-def simple_interest(principal: float, interest: float, duration_in_years: int):
+def simple_interest(principal: float, interest: float, duration_in_years: int) -> None:
     """A simple interest formula to print gain using the parameters provided.
     """
     print(f"Your investment: ${principal * (interest / 100) * duration_in_years + principal} "
