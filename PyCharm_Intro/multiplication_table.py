928dd1c0f9ef2e3938e36816feb40d1aae9d2739
Troy Martin <beef.erikson.studios@gmail.com>
2018-11-10 21:06:01 -0500

    added defaults to (only) function
    Signed-off-by: Troy Martin <beef.erikson.studios@gmail.com>

--- PyCharm_Intro/multiplication_table.py
+++ PyCharm_Intro/multiplication_table.py
@@ -2,7 +2,7 @@
 """
 
 
-def print_multiplication_table(number_to_multiply: int, start: int, end: int) -> None:
+def print_multiplication_table(number_to_multiply: int=5, start: int=1, end: int=10) -> None:
     """Creates a multiplication table based on number and a start/end point
     """
     for index in range(start, end + 1):
@@ -10,3 +10,4 @@ def print_multiplication_table(number_to_multiply: int, start: int, end: int) ->
 
 
 print_multiplication_table(6, 34, 39)
+print_multiplication_table()
