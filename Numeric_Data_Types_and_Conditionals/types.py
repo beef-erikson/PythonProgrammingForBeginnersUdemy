dfe27608664f44a5693f6059ccdcade5cf73a372
Troy Martin <beef.erikson.studios@gmail.com>
2018-11-10 22:29:40 -0500

    added types
    Signed-off-by: Troy Martin <beef.erikson.studios@gmail.com>

---
+++ Numeric_Data_Types_and_Conditionals/types.py
@@ -0,0 +1,10 @@
+"""Demo of how Python handles types
+"""
+
+number = 5
+value = 2.5
+
+print(type(number))     # int
+print(type(value))      # float
+print(type("wheee"))    # str
+print(type(6/2))        # float
